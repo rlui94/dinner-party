42e8baf2ab9985e1fb079c7c7c93eab287d80c9e
ryant <ryanlui.college@gmail.com>
2019-10-21 02:14:21 -0700

    Coded readfile to read in data from prepared txt file.

--- data.py
+++ data.py
@@ -1,7 +1,22 @@
 class Data:
-    """Contains information on party members."""
+    """Contains given information on party members."""
 
     def __init__(self):
         self.members = 0
         self.guests = 0
         self.hosts = 0
+        self.prefmatrix = []
+
+    def readfile(self, filename):
+        """Reads a file in the format described in the README and stores info"""
+        with open(filename, 'r') as file:
+            self.members = int(file.readline())
+            # first line is always an even number, this is our number of members
+            for line in file:
+                self.prefmatrix.append(
+                    list(int(num) for num in line.split())
+                )
+            # For each line, create new list of numbers cast from string to int
+            # Append to the empty preference matrix to create full prefmatrix
+            # https://stackoverflow.com/a/6583635
+        print(self.prefmatrix[1][3])
